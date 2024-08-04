7977e7f80c59fad087e504caaebddbcd5427dcda
Raphael Feitosa <raphael.feitosa@zup.com.br>
2024-08-04 19:36:31 -0300

    feat: adjust validations updated balance

--- src/main/kotlin/caju/tech/transactionauthorizer/domain/BalanceAccount.kt
+++ src/main/kotlin/caju/tech/transactionauthorizer/domain/BalanceAccount.kt
@@ -2,7 +2,7 @@ package caju.tech.transactionauthorizer.domain
 
 data class BalanceAccount(
     val accountId: String,
-    val food: Double,
-    val meal: Double,
-    val cash: Double,
+    val food: Double?,
+    val meal: Double?,
+    val cash: Double?,
 )
