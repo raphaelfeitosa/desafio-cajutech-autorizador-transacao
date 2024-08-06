10813855c30f77740c6c64b9040f61f300596abe
Raphael Feitosa <raphael.feitosa@zup.com.br>
2024-08-06 11:03:29 -0300

    feat: adding lock transaction with redis

--- src/main/kotlin/caju/tech/transactionauthorizer/adapter/ports/input/exceptions/errors/Errors.kt
+++ src/main/kotlin/caju/tech/transactionauthorizer/adapter/ports/input/exceptions/errors/Errors.kt
@@ -4,4 +4,5 @@ object Errors {
     const val METHOD_ARGUMENT_NOT_VALID = "Method argument not valid"
     const val RESOURCE_NOT_FOUND = "Resource not found"
     const val DOCUMENT_NUMBER_ALREADY_EXISTS = "Document number already registered"
+    const val RESOURCE_ALREADY_LOCKED = "Resource already locked"
 }
